e3ddc029f665e2f25657f519378f75a77704efac
Michael Do <michael.do@shironeko.info>
2020-05-25 15:33:25 -0700

    Updates

---
+++ precache-manifest.d67db476eb754bec46c021a0188acd0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f622fbb29c6a7e01de496c2a638d1354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9badfcfbf76e0570f216",
+    "url": "/static/css/main.d5b7c0ae.chunk.css"
+  },
+  {
+    "revision": "0c4e7a3250e898e6244b",
+    "url": "/static/js/2.387c18cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.387c18cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9badfcfbf76e0570f216",
+    "url": "/static/js/main.46f9cbed.chunk.js"
+  },
+  {
+    "revision": "f98298b200a6babb26a7",
+    "url": "/static/js/runtime-main.012eef2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
